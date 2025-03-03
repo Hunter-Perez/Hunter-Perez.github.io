7e30cbbbfe64398990a590a754e149669dbb28e8
Hunter Perez <hperez@bluebridgefinancial.com>
2025-03-03 15:49:08 -0500

    commit

--- equibase_scrape/script.js
+++ equibase_scrape/script.js
@@ -97,11 +97,11 @@ function populateTable(data) {
   //const headers = Object.keys(data[0]);
 
   // Create table headers statically
-  const headers = ['rank', 'horseName', 'sireName', 'starts', 'win', 'place', 'show', 'earnings', 'perStart', 'winPercentage', 'speedFigure']
+  const headers = ['rank', 'horseName', 'sireName', 'starts', 'win', 'place', 'show', 'earnings', 'perStart', 'winPercentage', 'topThree', 'topThreePercentage', 'speedFigure']
   //const headers = ['rank', 'winPercentage', 'horseName', 'referenceNumber', 'earnings', 'place', 'sireReferenceNumber', 'starts', 'sireName', 'speedFigure', 'perStart', 'topThreePercentage', 'show', 'win', 'topThree', 'registry']
   headers.forEach(header => {
     const th = document.createElement('th');
-    const labelMap = {'rank': 'Rank', 'horseName': 'Horse Name', 'sireName': 'Sire Name', 'starts': 'Sts', 'win': '1st', 'place': '2nd', 'show': '3rd', 'earnings': 'Total $', 'perStart': 'Per Start $', 'winPercentage': 'Win%', 'speedFigure': 'Speed Figure'}
+    const labelMap = {'rank': 'Rank', 'horseName': 'Horse Name', 'sireName': 'Sire Name', 'starts': 'Sts', 'win': '1st', 'place': '2nd', 'show': '3rd', 'earnings': 'Total $', 'perStart': 'Per Start $', 'winPercentage': 'Win%', 'topThree': 'Top3', 'topThreePercentage': 'Top3%', 'speedFigure': 'Speed Figure'}
     th.textContent = labelMap[header];
     tableHeaders.appendChild(th);
   });
